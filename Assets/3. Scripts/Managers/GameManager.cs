8ba6082004a1c22af51b804045a48aa34b39f013
hio0 <hioohio1234@naver.com>
2026-06-12 03:23:31 +0900

    s

--- Assets/3. Scripts/Managers/GameManager.cs
+++ Assets/3. Scripts/Managers/GameManager.cs
@@ -5,24 +5,71 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject qner;
-    public GameObject anser;
+    public static GameManager gm;
 
+    public GameObject qner;
+    int Qcount;
+    public TMP_Text QcountT;
+    public Question[] questions;
+    public Question nowQ;
+    public TMP_Text qT;
+
+    public GameObject anser;
     public GameObject pre_AnswerB;
+
+    bool isquestionSet;
+    int days;
+
+    private void Awake()
+    {
+        gm = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        UIEvent.Event.OnAnswerClicked += Answerd;
+
+        ResetGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isquestionSet)
+        {
+            SetQuestion();
+            isquestionSet = true;
+        }
+    }
+
+    public void ResetGame()
+    {
+        days = 0;
+        Qcount = 0;
+
+        qner.SetActive(false);
+        isquestionSet = false;
+    }
+
+    public void NextDay()
+    {
+        days++;
     }
 
     public void SetQuestion()
+    {
+        qner.SetActive(true);
+        Qcount++;
+        QcountT.text = Qcount.ToString();
+
+        int r = Random.Range(0, questions.Length);
+        nowQ = questions[r];
+
+        qT.text = nowQ.Qtext;
+    }
+
+    public void Answerd()
     {
 
     }
--- Assets/3. Scripts/Objects/AnswerButton.cs
+++ Assets/3. Scripts/Objects/AnswerButton.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AnswerButton : MonoBehaviour
 {
+    public TMP_Text text;
+   
     // Start is called before the first frame update
     void Start()
     {

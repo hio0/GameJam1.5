37f9f698c17719a05fc84e58403a3ffa55430d0d
hio0 <hioohio1234@naver.com>
2026-06-14 20:27:26 +0900

    q

--- Assets/3. Scripts/Objects/Book.cs
+++ Assets/3. Scripts/Objects/Book.cs
@@ -30,6 +30,12 @@ public class Book : MonoBehaviour
     public GameObject pre_mokchaA;
     public GameObject pre_mokchaB;
 
+    private void OnEnable()
+    {
+        SetMokcha();
+        nowpage = 0;
+    }
+
     void Update()
     {
         if (!isFlipping)
@@ -50,15 +56,16 @@ public class Book : MonoBehaviour
         text.text = $"P.{much}|{part}";
     }
 
-    void SetMokcha()
+    public void SetMokcha()
     {
         A_Text.text = null;
         B_Text.text = null;
         C_Text.text = null;
 
-        //A_PT.text = 
-
-        //Instantiate(pre_mokchaA, A_content);
+        if (A_content.childCount == 0)
+        {
+            Instantiate(pre_mokchaA, A_content);
+        }
     }
 
     IEnumerator FlipPage(bool forward)
@@ -80,12 +87,76 @@ public class Book : MonoBehaviour
             yield return null;
         }
 
+        Vector2 vec = Vector2.zero;
+        if (forward)
+        {
+            vec = new Vector2(320f, -72.5f);
+
+            if (nowpage + 1 > pages.Length)
+            {
+                StopAllCoroutines();
+            }
+            else
+            {
+                A_Text.text = B_Text.text;
+
+                if (nowpage == 0)
+                {
+                    Destroy(A_content.transform.GetChild(0).gameObject);
+
+                    B_Text.text = pages[nowpage].text;
+                    C_Text.text = pages[nowpage + 1].text;
+                }
+                else
+                {
+                    if (nowpage + 1 >= pages.Length)
+                    {
+                        StopAllCoroutines();
+                    }
+                    else
+                    {
+                        B_Text.text = pages[nowpage + 1].text;
+                    }
+                    if (nowpage + 2 >= pages.Length)
+                    {
+                        pageC.SetActive(false);
+                        StopAllCoroutines();
+                    }
+                    else
+                    {
+                        pageC.SetActive(true);
+                        C_Text.text = pages[nowpage + 2].text;
+                    }
+                }
+
+                nowpage++;
+
+            }
+        }
+        else
+        {
+            vec = new Vector2(-320f, -72.5f);
+
+            pageC.SetActive(true);
+            nowpage--;
+
+            C_Text.text = B_Text.text;
+
+            B_Text.text = pages[nowpage].text;
+            if (nowpage == 0)
+            {
+                SetMokcha();
+            }
+        }
+        pageA.anchoredPosition = vec;
+
         ApplyFlip(end, forward);
         isFlipping = false;
     }
 
     void ApplyFlip(float t, bool forward)
     {
+
         t = Mathf.SmoothStep(0, 1, t);
 
         // 1️⃣ 회전
@@ -109,6 +180,10 @@ public class Book : MonoBehaviour
                 pageA.localScale = new Vector3(-1, 1, 1);
             else
                 pageA.localScale = new Vector3(1, 1, 1);
+            /*
+            A_Text.text = B_Text.text;
+            B_Text.text = C_Text.text;
+        */
         }
         else
         {
